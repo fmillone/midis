454d3fcaedf4de90027642121dfb688eb21fbc41
Francisco Millone <franba2010@gmail.com>
2016-07-12 22:47:25 -0300

    added octave from frequency method

--- src/main/java/org/hzbrainz/Main.java
+++ src/main/java/org/hzbrainz/Main.java
@@ -1,9 +1,10 @@
 package org.hzbrainz;
 
-
 public class Main {
 
     public static void main(String[] args){
         System.out.println("test");
+
+        System.out.println(Utils.octaveFromHz(440.0));
     }
 }
--- src/main/java/org/hzbrainz/Utils.java
+++ src/main/java/org/hzbrainz/Utils.java
@@ -6,11 +6,21 @@ package org.hzbrainz;
 public class Utils {
 
     private static final double A_FREQUENCY = 440.0;
+    private static final double C4_FREQUENCY = 261.6255653005986346778499935233047013;
     private static final double CONSTANT = 1.05946309436;
+    private static final int TWO_POWER_SIXTEEN = 16;
 
     public static Note noteFromHz(double hz){
-        int step = (int)(Math.log(hz/ A_FREQUENCY)/Math.log(CONSTANT));
+        int step = (int)(Math.log(hz/ C4_FREQUENCY)/Math.log(CONSTANT));
         return Note.A.noteFromStep(step);
     }
 
+    public static int octaveFromHz(double hz){
+        return (int) (Math.log(hz/getPrimalFreq())/Math.log(2));
+    }
+
+
+    private static double getPrimalFreq() {
+        return C4_FREQUENCY/ TWO_POWER_SIXTEEN;
+    }
 }
