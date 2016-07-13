d32ab5376d7c7be97fcba76a9c83287d2582d73a
Francisco Millone <franba2010@gmail.com>
2016-07-12 23:11:25 -0300

    toma pa vo'

--- src/main/java/org/hzbrainz/Main.java
+++ src/main/java/org/hzbrainz/Main.java
@@ -1,10 +1,42 @@
 package org.hzbrainz;
 
+import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
+import org.jfugue.rhythm.Rhythm;
+import org.jfugue.theory.ChordProgression;
+
 public class Main {
 
     public static void main(String[] args){
         System.out.println("test");
 
         System.out.println(Utils.octaveFromHz(440.0));
+
+        Player player = new Player();
+        player.play("C4 D4 E4 F4 G4 A4 B4");
+        Pattern p1 = new Pattern("V0 I[Piano] Eq Ch. | Eq Ch. | Dq Eq Dq Cq");
+        Pattern p2 = new Pattern("V1 I[Flute] Rw     | Rw     | GmajQQQ  CmajQ");
+        player = new Player();
+        player.play(p1, p2);
+
+
+        Pattern pattern = new ChordProgression("I IV V")
+                .distribute("7%6")
+                .allChordsAs("$0 $0 $0 $0 $1 $1 $0 $0 $2 $1 $0 $0")
+                .eachChordAs("$0ia100 $1ia80 $2ia80 $3ia80 $4ia100 $3ia80 $2ia80 $1ia80")
+                .getPattern()
+                .setInstrument("Acoustic_Bass")
+                .setTempo(100);
+        new Player().play(pattern);
+
+        Rhythm rhythm = new Rhythm()
+                .addLayer("O..oO...O..oOO..")
+                .addLayer("..S...S...S...S.")
+                .addLayer("````````````````")
+                .addLayer("...............+");
+        new Player().play(rhythm.getPattern().repeat(2));
+
+        //http://www.jfugue.org/examples.html
+        //los comentarios en codigo están mal
     }
 }
